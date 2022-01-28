01ca7219600de383e0795ad235f43e7230430d1b
pranjaltechnical <pranjaltech.com>
2022-01-29 01:46:17 +0530

    heroku deployment

---
+++ src/db/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://NITSOLX:nitsolx@cluster0.2rfwt.mongodb.net/usermernstack?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Mongo db is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
